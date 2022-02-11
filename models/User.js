c478ee6c085cb12121aa04761abc88bcffe27a64
ingramwd <Ingramwd01@gmail.com>
2022-02-10 19:10:31 -0500

    set up relationships in models/index.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
 User.init(
     {
         id: {
-            type: DataTypes.INTEGER 
+            type: DataTypes.INTEGER
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         }
     }
 )
--- models/index.js
+++ models/index.js
@@ -1,10 +1,20 @@
 const Tv = require('./Tv');
 const Review = require('./Review');
+const { associations } = require('./Tv');
 
 // TODO
 // Need TV and Review Assocations
 // TV hasMany Review
 // Review belongTo TV
+
+Tv.hasMany(Review, {
+    foreignKey: 'tv_id',
+});
+
+Review.belongsTo(Tv, {
+    foreignKey: 'tv_id',
+    onDelete: 'SET NULL'
+});
 
 // TODO 
 // User HasMany Review 
@@ -12,6 +22,14 @@ const Review = require('./Review');
 // Tv hasmany Review
 // review belongto tv?
 
+User.hasMany(Review, {
+    foreignKey: 'id'
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'id',
+    onDelete: 'SET NULL'
+});
 
 
 module.exports = { Tv, Review };
