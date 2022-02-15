fc70617356f8358fa646826f4d31967d9fea4d52
ingramwd <Ingramwd01@gmail.com>
2022-02-14 20:12:48 -0500

    added forntend js to singup and login pages

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,16 +1,28 @@
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    console.log('clicked');
-    const username = document.querySelector('#username').value.trim();
-    const password = document.querySelector('#password').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-    const response = await fetch(`/api/users`, {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
         }
-    });
+    }
 }
 
-document.querySelector('#loginSubmit').addEventListener('click', loginFormHandler);
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+
